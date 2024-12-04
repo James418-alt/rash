beb8c27c90dec49d4d5c517342eab35f780319da
James418-alt <ebifegha123@gmail.com>
2024-12-04 13:23:33 +0100

    update

--- utils/dbConfig.ts
+++ utils/dbConfig.ts
@@ -1,7 +1,8 @@
 import { log } from "console";
 import { connect } from "mongoose";
 export const dbConfig = async () => {
-  const url = "mongodb://localhost:27017/backdb";
+  const url =
+    "mongodb+srv://ebifegha123:ebifegha123@gomenticode.1l9lbmv.mongodb.net/rashdb?retryWrites=true&w=majority&appName=GomentiCode";
   await connect(url).then(() => {
     log("Server Up");
   });
